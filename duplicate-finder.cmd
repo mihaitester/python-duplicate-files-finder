b6eede296eef8d07075caab319c5d5005afd6692
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-06 10:41:20 +0300

    fix(script): passed in all parameters from commandline

--- duplicate-finder.cmd
+++ duplicate-finder.cmd
@@ -1,3 +1,3 @@
 rem default use script that will dump the `.cache` file containing metadata and the `.json` file containing duplicates
-rem replace `"%cd%"` with a drive path like `c:\` or `d:\` or include both `c:\ d:\`
-python duplicate-finder.py -j -c "%cd%"
+rem replace `%*` with a drive path like `c:\` or `d:\` or include both `c:\ d:\` or pass them through the `.cmd` script
+python duplicate-finder.py -j -c %*
