b5b1591dd27b339dfc2add6589f0b39aa9c7e619
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2023-12-15 09:54:16 +0200

    fix(launch): improved displayed information from batch script

--- duplicate-finder.cmd
+++ duplicate-finder.cmd
@@ -1,3 +1,7 @@
-rem default use script that will dump the `.cache` file containing metadata and the `.json` file containing duplicates
-rem replace `%*` with a drive path like `c:\` or `d:\` or include both `c:\ d:\` or pass them through the `.cmd` script
-python duplicate-finder.py -j -c %*
+@echo off
+echo ^>^>^> default use script that will dump the `.cache` file containing metadata and the `.json` file containing duplicates
+echo ^>^>^> replace `%*` with a drive path like `c:\` or `d:\` or include both `c:\ d:\` or pass them through the `.cmd` script
+
+set "command=python duplicate-finder.py -p -j -c %*"
+echo %command%
+%command%
