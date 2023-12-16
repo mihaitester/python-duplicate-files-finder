9a906d79b1160391c6e95626722de686475c3bb3
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2023-12-16 19:42:20 +0200

    fix(launch): removed -p flag so that both methods can be tested

--- duplicate-finder.cmd
+++ duplicate-finder.cmd
@@ -2,6 +2,6 @@
 echo ^>^>^> default use script that will dump the `.cache` file containing metadata and the `.json` file containing duplicates
 echo ^>^>^> replace `%*` with a drive path like `c:\` or `d:\` or include both `c:\ d:\` or pass them through the `.cmd` script
 
-set "command=python duplicate-finder.py -p -j -c %*"
+set "command=python duplicate-finder.py -j -c %*"
 echo %command%
 %command%
