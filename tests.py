ed3d09d7a0f0d4012f89c976ffae221c432427fb
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2023-12-20 12:04:46 +0200

    feat(test): added skeleton for testing script

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestDuplicateFinder(unittest.TestCase):
+
+    def test_single_file(self):
+        print("works")
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
