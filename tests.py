bed8fe36be48496d300f17cd5acd9ded8dfc8e23
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2023-12-20 14:46:29 +0200

    fix(thread): added more prints and found that not all the output gets collected, although the thread does collect all output

--- tests.py
+++ tests.py
@@ -5,13 +5,14 @@ import unittest
 import subprocess
 import threading
 import time
+# import copy
 
 BASE_FOLDER = os.path.abspath(os.path.dirname(__file__))
 TEST_FOLDER = os.path.join(BASE_FOLDER, "scrap_test_folder")
 SCRIPT = "duplicate-finder.cmd"
 ENCODING = "UTF-8"
 
-THREAD_LOCK = threading.Lock()
+# THREAD_LOCK = threading.Lock() # note: this is not needed because there is a single reading thread, and the tests are sequential and processes launched are being waited for
 THREAD_STDOUT = b""
 THREAD_STDERR = b""
 THREAD_SLEEP = 2
@@ -28,15 +29,15 @@ def thread_reader(stdout_pipe, stderr_pipe, finished):
             stdout = stdout_pipe.read()
             stderr = stderr_pipe.read()
             if stdout:
-                THREAD_LOCK.acquire()
+                # THREAD_LOCK.acquire()
                 THREAD_STDOUT = THREAD_STDOUT + stdout
-                THREAD_LOCK.release()
+                # THREAD_LOCK.release()
                 # print(stdout)
                 # print(THREAD_STDOUT)
             if stderr:
-                THREAD_LOCK.acquire()
+                # THREAD_LOCK.acquire()
                 THREAD_STDERR = THREAD_STDERR + stderr
-                THREAD_LOCK.release()
+                # THREAD_LOCK.release()
                 # print(stderr)
                 # print(THREAD_STDERR)
         except Exception as ex:
@@ -45,42 +46,52 @@ def thread_reader(stdout_pipe, stderr_pipe, finished):
 
 def run_command_and_get_output(command):
     # note: this does not properly output the results from sub processes opened, makes me think that python is hacked and that processes are virtualized instead of actually running in their own shell
-    stderr = b""
-    stdout = b""
+    stderr = ""
+    stdout = ""
+    rc = 0
 
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # note: this fixes handles but it does not return the full output from process
+    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        # note: this pollutes stdout with both stdout and stderr    
+        # while True:
+        #     if proc.poll() is not None:
+        #         break
+        #     stdout += proc.stdout.read()
+        #     stderr += proc.stderr.read()
+        try:
+            reader_finished = threading.Event()
+            reader_thread = threading.Thread(target=thread_reader, args=(proc.stdout, proc.stderr, reader_finished))
+            reader_thread.start()
+            proc.wait()
+            reader_finished.set()
+            reader_thread.join()
+        except Exception as ex:
+            print("Failed to start process reading thread with [{}]".format(ex.message))
+        # proc.kill()
+
+        # THREAD_LOCK.acquire()
+        stdout = THREAD_STDOUT.decode(ENCODING)
+        stderr = THREAD_STDERR.decode(ENCODING)
+        rc = proc.returncode
+        # THREAD_LOCK.release()
+        
+        # try:
+        #     stdout, stderr = proc.communicate(timeout=15)
+        #     while True:
+        #         if process.poll() is not None:
+        #             break
+        #         data = process.stdout.readline()
+        #         print data
+        # except TimeoutExpired:
+        #     proc.kill()
+        #     stdout, stderr = proc.communicate()
     
-    # note: this pollutes stdout with both stdout and stderr    
-    # while True:
-    #     if proc.poll() is not None:
-    #         break
-    #     stdout += proc.stdout.read()
-    #     stderr += proc.stderr.read()
-    try:
-        reader_finished = threading.Event()
-        reader_thread = threading.Thread(target=thread_reader, args=(proc.stdout, proc.stderr, reader_finished))
-        reader_thread.start()
-        proc.wait()
-        reader_finished.set()
-        reader_thread.join()
-    except Exception as ex:
-        print("Failed to start process reading thread with [{}]".format(ex.message))
+    # note: these work, but outside this global method, the test class fails to be used properly
+    # print(stdout)
+    # print(stderr)
+    # print(rc)
+    return stdout, stderr, rc
 
-    stdout = THREAD_STDOUT
-    stderr = THREAD_STDERR
-
-    # try:
-    #     stdout, stderr = proc.communicate(timeout=15)
-    #     while True:
-    #         if process.poll() is not None:
-    #             break
-    #         data = process.stdout.readline()
-    #         print data
-    # except TimeoutExpired:
-    #     proc.kill()
-    #     stdout, stderr = proc.communicate()
-
-    return stdout.decode(ENCODING), stderr.decode(ENCODING), proc.returncode
 
 class TestDuplicateFinder(unittest.TestCase):
 
@@ -94,7 +105,7 @@ class TestDuplicateFinder(unittest.TestCase):
 
     def test_single_file(self):
         # os.system(SCRIPT + " " + TEST_FOLDER) # todo: remove console print pollution from running tests
-        stdout, stderr, rc = run_command_and_get_output(SCRIPT + " " + TEST_FOLDER)
+        stdout, stderr, rc = run_command_and_get_output(SCRIPT + " " + TEST_FOLDER) # todo: figure out why this fails [ ResourceWarning: Enable tracemalloc to get the object allocation traceback ]
         print(stdout)
         pass
 
